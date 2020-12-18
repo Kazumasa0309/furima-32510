2625fcb390c56a24e4df22f17aa8acd5f2aff8c1
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-18 12:17:18 +0900

    新規登録実装＆Userモデルバリデーション無効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  valid_password_hankaku = /\A[a-z0-9]+\z/i
-  valid_password_zenkaku = /\A[ぁ-んァ-ン一-龥]/
-  valid_password_zenkaku_kana = /\A[ァ-ヶー－]+\z/
-  message_zenkaku = "お名前は、全角で入力してください。"
-  message_zenkaku_kana = "お名前カナは、全角カタカナで入力してください。"
-  with_options presence: true do
-    validates :nickname
-    validates :email,              uniqueness: true
-    validates :encrypted_password, length: { minimum: 6 }, format: { with: valid_password_hankaku }
-    validates :first_name,         format: { with: valid_password_zenkaku, message: message_zenkaku }
-    validates :last_name,          format: { with: valid_password_zenkaku, message: message_zenkaku }
-    validates :first_name_kana,    format: { with: valid_password_zenkaku_kana, message: message_zenkaku_kana }
-    validates :last_name_kana,     format: { with: valid_password_zenkaku_kana, message: message_zenkaku_kana }
-    validates :birthday
-  end
+  # valid_password_hankaku = /\A[a-z0-9]+\z/i
+  # valid_password_zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  # valid_password_zenkaku_kana = /\A[ァ-ヶー－]+\z/
+  # message_zenkaku = "お名前は、全角で入力してください。"
+  # message_zenkaku_kana = "お名前カナは、全角カタカナで入力してください。"
+  # with_options presence: true do
+  #   validates :nickname
+  #   validates :email,              uniqueness: true
+  #   validates :encrypted_password, length: { minimum: 6 }, format: { with: valid_password_hankaku }
+  #   validates :first_name,         format: { with: valid_password_zenkaku, message: message_zenkaku }
+  #   validates :last_name,          format: { with: valid_password_zenkaku, message: message_zenkaku }
+  #   validates :first_name_kana,    format: { with: valid_password_zenkaku_kana, message: message_zenkaku_kana }
+  #   validates :last_name_kana,     format: { with: valid_password_zenkaku_kana, message: message_zenkaku_kana }
+  #   validates :birthday
+  # end
 end
