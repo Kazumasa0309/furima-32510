a6e5220af79068e0d9f81588bef2db96cf795b6b
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-19 12:36:13 +0900

    itemバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :user
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :price,             numericality: { :only_integer true,
+                                                  :greater_than_or_equal_to 300,
+                                                  :less_than_or_equal_to 9999999
+                                                }
+  end
 end
