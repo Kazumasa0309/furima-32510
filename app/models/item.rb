a937c264d4ce0d59883cd84f7a9c38316aeddff3
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-20 11:53:32 +0900

    プルダウン_発送日実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,8 @@ class Item < ApplicationRecord
     belongs_to :category
     belongs_to :status
     belongs_to :delivery_fee
-    belongs_to :delivery_fee
+    belongs_to :prefecture
+    belongs_to :delivery_day
 
   with_options presence: true do
     validates :user
@@ -16,10 +17,10 @@ class Item < ApplicationRecord
     validates :status_id,       numericality: { other_than: 1 }
     validates :delivery_fee_id, numericality: { other_than: 1 }
     validates :prefecture_id,   numericality: { other_than: 1 }
-    validates :delivery_day_id
-    validates :price,             numericality: { :only_integer true,
-                                                  :greater_than_or_equal_to 300,
-                                                  :less_than_or_equal_to 9999999
-                                                }
+    validates :delivery_day_id, numericality: { other_than: 1 }
+    validates :price,           numericality: { :only_integer true,
+                                                :greater_than_or_equal_to 300,
+                                                :less_than_or_equal_to 9999999
+                                              }
   end
 end
