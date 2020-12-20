94e47088b681063dbad441b1c4604bf858de5587
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-20 18:48:39 +0900

    Itemモデルバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,13 @@ class Item < ApplicationRecord
     validates :user
     validates :name,            length: { maximum: 40 }
     validates :explanation
-    validates :category_id,     numericality: { other_than: 1 }
-    validates :status_id,       numericality: { other_than: 1 }
-    validates :delivery_fee_id, numericality: { other_than: 1 }
-    validates :prefecture_id,   numericality: { other_than: 1 }
-    validates :delivery_day_id, numericality: { other_than: 1 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :delivery_day_id
+    end
     validates :price,           numericality: { only_integer: true,
                                                 greater_than_or_equal_to: 300,
                                                 less_than_or_equal_to: 9999999
