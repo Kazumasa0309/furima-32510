4078aa4cd55b48ca84bbf153057a74d2672531f2
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-24 15:54:47 +0900

    商品購入実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   end
  
   def create
-    binding.pry
+    @item = Item.find(params[:item_id])
     @order_receiver = OrderReceiver.new(order_params)
      if @order_receiver.valid?
        pay_item
@@ -20,13 +20,13 @@ class OrdersController < ApplicationController
   private
    # 全てのストロングパラメーターを1つに統合
   def order_params
-   params.require(:order_receiver).permit(:user, :item, :order, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(token: params[:token])
+   params.require(:order_receiver).permit(:order, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number).merge(item_id: params[:item_id], token: params[:token], user_id: current_user.id)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                # 通貨の種類（日本円）
     )
--- app/models/order_receiver.rb
+++ app/models/order_receiver.rb
@@ -1,13 +1,13 @@
 class OrderReceiver
   include ActiveModel::Model
-  attr_accessor :user, :item, :order, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :token
+  attr_accessor :user_id, :item_id, :order, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :token
 
   with_options presence: true do
     # orderモデルのバリデーション
-    validates :user
-    validates :item
+    # validates :user
+    # validates :item
     # receiverモデルのバリデーション
-    validates :order
+    # validates :order
     validates :postal_code
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
@@ -18,8 +18,8 @@ class OrderReceiver
 
   def save
     # 注文の情報を保存し、「order」という変数に入れる
-    order = Order.create(user: @item.user.id, item: @item.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     # 送付先の情報を保存
-    Receiver.create(order: order.id, postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, phone_number: phone_number)
+    Receiver.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, phone_number: phone_number)
   end
 end
