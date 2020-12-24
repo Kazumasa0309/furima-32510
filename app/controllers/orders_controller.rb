5175bb1877d0120e5650c1c5253998eb76ab945e
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-24 18:52:20 +0900

    ログイン制御

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @order_receiver = OrderReceiver.new
+    @order = Order.where(item_id: params[:item_id])
+    move_to_index
   end
  
   def create
@@ -26,10 +30,15 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
+      amount: @item.price,           # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                # 通貨の種類（日本円）
     )
   end
 
+  def move_to_index
+    if (user_signed_in? && current_user.id == @item.user_id) || (user_signed_in? && !@order.empty? )
+      redirect_to root_path
+    end
+  end
 end
