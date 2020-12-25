99b4e193a407344d019f00c66eca9fbb877a7a75
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-25 16:09:19 +0900

    単体テストコード簡略化、その他

--- spec/factories/order_receivers.rb
+++ spec/factories/order_receivers.rb
@@ -1,9 +1,6 @@
 FactoryBot.define do
   factory :order_receiver do
     token         { "tok_abcdefghijk00000000000000000" }
-    user_id       {  }
-    item_id       {  }
-    order_id      {  }
     postal_code   { "120-0000" }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     municipality  { "Adachi-ku" }
